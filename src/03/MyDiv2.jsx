700b9b743080f335e83183282ff82a335d2a7527
pnumin <pnumin@pusan.ac.kr>
2025-02-27 09:33:52 +0900

    props

--- src/03/MyDiv2.jsx
+++ src/03/MyDiv2.jsx
@@ -6,7 +6,7 @@ export default function MyDiv2(props) {
                     p-10 font-bold
                     flex flex-col justify-start items-center">
       <div className="w-full text-left mb-10">
-        {props.md1} > {props.md2} 
+        {`${props.md1} > ${props.md2}`} 
       </div>
       <MyDiv3 d1={props.md1} d2={props.md2} d3={props.md3} />
     </div>
