c85e31bbfaf270a5effb0e0fad3264f65074e454
pnumin <pnumin@pusan.ac.kr>
2025-02-27 10:53:51 +0900

    props 예제제

--- src/04/MyList.jsx
+++ src/04/MyList.jsx
@@ -7,9 +7,11 @@ export default function MyList() {
     "content": "HTML(HyperText Markup Language)은 웹을 이루는 가장 기초적인 구성 요소로, 웹 콘텐츠의 의미와 구조를 정의할 때 사용"
   }
   return (
-    <div>
-      MyList
-      <MyListItem />
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 px-10 lg:px-2">
+      <MyListItem title={data.title} imgUrl={data.imgUrl} content={data.content} />
+      <MyListItem title={data.title} imgUrl={data.imgUrl} content={data.content} />
+      <MyListItem title={data.title} imgUrl={data.imgUrl} content={data.content} />
+      <MyListItem title={data.title} imgUrl={data.imgUrl} content={data.content} />
     </div>
   )
 }
--- src/04/MyListItem.jsx
+++ src/04/MyListItem.jsx
@@ -1,8 +1,23 @@
 
-export default function MyListItem() {
+export default function MyListItem({title, imgUrl, content}) {
+  let cnt = 0 ;
+  console.log(title, imgUrl, content)
   return (
-    <div>
-      
+    <div className="w-full h-full flex border border-gray-300">
+      <div className="w-1/4">
+        <img src={imgUrl} alt={title} />
+      </div>
+      <div className="w-3/4 ml-10">
+        <h1 className="h-1/4 text-2xl font-bold flex justify-start items-start pt-5" >
+          {title}
+        </h1>
+        <p className="h-2/4 text-lg">
+          {content}
+        </p>
+        <div className="h-1/4 text-xl font-bold flex justify-end items-end px-5 py-8">
+          ❤️ 좋아요 {cnt}
+        </div>
+      </div>
     </div>
   )
 }
