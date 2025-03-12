20574e57f5198f249ddcb20fbfa4b12d1df04110
pnumin <pnumin@pusan.ac.kr>
2025-03-12 11:09:08 +0900

    박스오피스

--- src/09/BoxOffice.jsx
+++ src/09/BoxOffice.jsx
@@ -1,5 +1,6 @@
 //table참고 : https://flowbite.com/docs/components/tables/
 import { useState, useEffect } from "react"
+import { FaArrowUp, FaArrowDown } from "react-icons/fa6"; 
 
 export default function BoxOffice() {
   //화면에 랜더링 될 상태 변수
@@ -29,7 +30,7 @@ export default function BoxOffice() {
     let dt = getYesterday().replaceAll('-', '');
 
     let url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
-    url = `${url}key=${mvApiKey}&targetDt=${dt}`;
+    url = `${url}key=${mvApiKey}&targetDt=${"20250301"}`;
 
     //console.log(url)
     const resp = await fetch(url);
@@ -37,6 +38,33 @@ export default function BoxOffice() {
 
     let boxList = data.boxOfficeResult.dailyBoxOfficeList;
     console.log(boxList)
+
+    let tm = boxList.map(item => <tr  key={item.movieCd}
+                                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                      <td className="w-4 p-4">
+                                        {item.rank}
+                                      </td>
+                                      <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {item.movieNm}
+                                      </td>
+                                      <td className="px-6 py-4 text-right">
+                                        {parseInt(item.salesAmt).toLocaleString()}
+                                      </td>
+                                      <td className="px-6 py-4 text-right">
+                                        {parseInt(item.audiCnt).toLocaleString()}
+                                      </td>
+                                      <td className="px-6 py-4 text-right">
+                                        {parseInt(item.salesAcc).toLocaleString()}
+                                      </td>
+                                      <td className="px-6 py-4 text-right">
+                                        {parseInt(item.audiAcc).toLocaleString()}
+                                      </td>
+                                      <td className="px-6 py-4 text-center inline-flex justify-center items-center">
+                                        { parseInt(item.rankInten) > 0 ? <span className="text-red-600"><FaArrowUp /></span> 
+                                          : parseInt(item.rankInten) < 0 ? <span className="text-blue-600"><FaArrowDown /></span> : ''}
+                                        { item.rankInten == 0 ? '-' : Math.abs(item.rankInten)}                                    </td>
+                                    </tr>);
+    setTags(tm) ;
   }
 
   //컴포넌트가 실행될때 한번 fetch
@@ -45,73 +73,39 @@ export default function BoxOffice() {
   }, []);
 
   return (
-    <div>
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <td className="p-4">              
-              </td>
-              <td className="px-6 py-3">
-                Product name
-              </td>
-              <th className="px-6 py-3">
-                Color
-              </th>
-              <th className="px-6 py-3">
-                Category
-              </th>
-              <th className="px-6 py-3">
-                Price
-              </th>
-              <th class="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td className="w-4 p-4">
-                 
-              </td>
-              <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </td>
-              <td class="px-6 py-4">
-                Silver
-              </td>
-              <td class="px-6 py-4">
-                Laptop
-              </td>
-              <td class="px-6 py-4">
-                $2999
-              </td>
-              <td class="px-6 py-4">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
-            </tr>   
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td className="w-4 p-4">
-                 
-              </td>
-              <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </td>
-              <td class="px-6 py-4">
-                Silver
-              </td>
-              <td class="px-6 py-4">
-                Laptop
-              </td>
-              <td class="px-6 py-4">
-                $2999
-              </td>
-              <td class="px-6 py-4">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-              </td>
-            </tr>          
-          </tbody>
-        </table>
-        
-      </div>
+    <div className="w-11/12 ">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead className="text-md font-bold text-gray-900 bg-gray-50
+                            border-b-2">
+          <tr>
+            <td className="p-4 w-16">
+              순위
+            </td>
+            <td className="px-6 py-3">
+              영화명
+            </td>
+            <th className="px-6 py-3">
+              매출액
+            </th>
+            <th className="px-6 py-3">
+              관객수
+            </th>
+            <th className="px-6 py-3">
+              누적매출액
+            </th>
+            <th className="px-6 py-3">
+              누적관객수
+            </th>
+            <th className="px-6 py-3 w-24">
+              증감률
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tags}
+        </tbody>
+      </table>
+
+    </div>
   )
 }
