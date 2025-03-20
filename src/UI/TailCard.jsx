f5f944fca9a39f27071584096bc2fcd46230bc56
pnumin <pnumin@pusan.ac.kr>
2025-03-20 12:38:53 +0900

    Card 컴포넌트

--- src/UI/TailCard.jsx
+++ src/UI/TailCard.jsx
@@ -2,11 +2,11 @@ export default function TailCard({ title, subtitle, imgurl, kws }) {
   return (
     <div className="max-w-sm bg-white border 
                    border-gray-200 rounded-lg shadow-sm">
-      <div>
-        <img className="rounded-t-lg" src={imgurl} alt={title} />
+      <div className="w-full h-48">
+        <img className="rounded-t-lg w-full h-full object-cover" src={imgurl} alt={title} />
       </div>
-      <div className="p-5 flex flex-col justify-end">
-        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+      <div className="p-5 flex flex-col justify-between">
+        <h1 className="h-10 mb-2 text-2xl font-bold tracking-tight text-gray-900 truncate">
           {title}
         </h1>
         <p className="mb-3 font-normal text-gray-700">
