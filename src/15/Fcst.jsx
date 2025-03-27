d28d85b1faf7e825fd31247ed04eeceea318acdb
pnumin <pnumin@pusan.ac.kr>
2025-03-27 10:58:11 +0900

    일기예보

---
+++ src/15/Fcst.jsx
@@ -0,0 +1,65 @@
+import TailButton from "../UI/TailButton" ;
+import TailSelect from "../UI/TailSelect";
+import getxy from "./getxy.json" ;
+
+import { useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Fcst() {
+  const refDt = useRef() ;
+  const refSi = useRef() ;
+  const navigate = useNavigate() ;
+
+  let opsSi = getxy.map(item => item["1단계"]) ;
+  opsSi = ["---시도선택---", ...opsSi] ;
+  
+
+  const handleChange = () => {
+    console.log(refSi.current.value)
+  }
+
+  const handleClick = (gubun) => {
+    if (refSi.current.value == "---시도선택---") {
+      alert("시도를 선택하세요.");
+      refSi.current.focus();
+      return;
+    }
+    const tm = getxy.filter( item => item["1단계"] == refSi.current.value)[0] ;
+    const x = tm["격자 X"] ;
+    const y = tm["격자 Y"] ;
+    console.log(tm)
+    console.log(refDt.current.value, refSi.current.value, gubun, x, y)
+    navigate(`/fcstlist?dt=${refDt.current.value}&si=${refSi.current.value}&gubun=${gubun}&x=${x}&y=${y}`);
+  }
+
+  useEffect(() => {
+    refDt.current.value = new Date().toISOString().split('T')[0];
+  }, []) ;
+  return (
+    <>
+    <h1 className="w-10/12 text-2xl font-bold
+                  flex justify-center items-center  my-10">
+      일기예보 선택
+    </h1>
+    <div className="w-10/12 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <input type="date"
+             ref = {refDt}
+             className="bg-gray-50 border border-gray-300
+             text-gray-900 text-lg rounded-lg  text-center
+             focus:ring-blue-500 focus:border-blue-500 block w-full p-1" />
+      <TailSelect id = "selSi"
+                  refSel = {refSi}
+                  ops = {opsSi}
+                  handleChange = {handleChange} />
+
+      <TailButton caption ="초단기 예보"
+                  color = "blue"
+                  onClick = {() => handleClick("초단기예보")} />
+
+      <TailButton caption ="단기 예보"
+                  color = "blue"
+                  onClick = {() => handleClick("단기예보")} />
+    </div>
+    </>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,110 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import Hello from "./01/Hello"
-// import MyClock from './02/MyClock';
+import MyClock from './02/MyClock';
 // import MyDiv1 from './03/MyDiv1';
 // import MyList from './04/MyList';
-//import Lotto from './05/Lotto';
-// import FoodMain from './06/FoodMain'; 
+import Lotto from './05/Lotto';
+import FoodMain from './06/FoodMain'; 
 // import MyToggle from './07/MyToggle';
 // import MyEffect from './08/MyEffect';
-// import BoxOffice from './09/BoxOffice';
-// import Traffic from './10/Traffic';
+import BoxOffice from './09/BoxOffice';
+import Traffic from './10/Traffic';
 // import MyRef from './11/MyRef';
-// import Gallery from './12/Gallery';
+import Gallery from './12/Gallery';
 import Festival from './13/Festival';
+// import RouteMain from './14/RouteMain';
+import Fcst from './15/Fcst';
+import FcstList from './15/FcstList';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaHome } from "react-icons/fa";
 function App() {
   return (
+    <BrowserRouter>
     <div className="w-full xl:w-8/10 h-screen mx-auto 
                     flex flex-col">
       <header className="w-full min-h-20 bg-lime-50
-                         px-10
+                         px-4
                          flex justify-between items-center">
 
-        <div className="text-4xl font-bold text-lime-700 flex">
+        <div className="text-xl font-bold text-lime-700 flex">
           PNU KDT10 
-          <div className="flex text-sm items-center mx-5">
+          <div className="flex text-sm items-center mx-2">
             <img src={reactLogo} alt="react" className="w-8" /> + 
             <img src={viteLogo} alt="vite" className="w-8" />
           </div>
         </div>
+        <ul className='flex justify-center items-center'>
+          <li>
+            <Link to="/lotto" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              로또
+            </Link>
+          </li>
+          <li>
+            <Link to="/food" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              푸드뱅크
+            </Link>
+          </li>
+          <li>
+            <Link to="/box" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              박스오피스
+            </Link>
+          </li>
+          <li>
+            <Link to="/traffic" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              교통사고
+            </Link>
+          </li>
+          <li>
+            <Link to="/gallery" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              부산관광
+            </Link>
+          </li>
+          <li>
+            <Link to="/festival" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              부산축제
+            </Link>
+          </li>
+          <li>
+            <Link to="/fcst" className='inline-flex px-4 py-2 m-1 font-bold
+                                         rounded-lg
+                                         hover:cursor-pointer hover:bg-amber-700 hover:text-white'>
+              일기예보
+            </Link>
+          </li>
+        </ul>
         <div className="text-3xl font-bold text-black">
-          <FaHome />
+          <Link to="/"><FaHome /></Link>
         </div>
       </header>
-      <main className="w-full flex-grow
-                      pt-10
+      <main className="w-full flex-grow py-4
                       flex flex-col justify-start items-center
                       overflow-y-auto">
-
-        <Festival />
+        <Routes>
+          <Route path='/' element={<MyClock />} />
+          <Route path='/lotto' element={<Lotto />} />
+          <Route path='/food' element={<FoodMain />} />
+          <Route path='/box' element={<BoxOffice />} />
+          <Route path='/traffic' element={<Traffic />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/festival' element={<Festival />} />
+          <Route path='/fcst' element={<Fcst />} />
+          <Route path='/fcstlist' element={<FcstList />} />
+        </Routes>
       </main>
       <footer className="w-full min-h-20 bg-lime-900
                          px-10 text-md text-lime-50 font-bold
@@ -47,6 +112,7 @@ function App() {
          [K-Digital 부산대 25-1회차] AI 데이터 분석 풀스택 웹 개발자 양성과정                 
       </footer>
     </div>
+    </BrowserRouter>
   )
 }
 
